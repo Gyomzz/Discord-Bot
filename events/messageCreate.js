f77be220f57243b0be52b5b5104722c94ec1488c
Gyommz <millet.g@hotmail.com>
2022-03-25 17:05:09 +0100

    setup bot with command and event

---
+++ events/messageCreate.js
@@ -0,0 +1,26 @@
+const fs = require('node:fs');
+
+module.exports = {
+	name: 'messageCreate',
+	once: false,
+	execute(message) {
+        console.log(`message from ${message.channel.type} by ${message.author} -> ${message}`);
+        if(message.content.toLowerCase() === '!help') {
+            try {
+                message.channel.send('Voici la liste des commandes')
+            } catch(e) {
+                    
+            }
+            const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+            for (const file of commandFiles) {
+                const command = require(`../commands/${file}`);
+                try {
+                    message.channel.send(`/${command.data.name} : ${command.data.description}`)
+                } catch(e) {
+                    
+                }
+            }
+           
+        }
+	}
+};
---
+++ readMe.md
@@ -0,0 +1,20 @@
+# Discord Bot
+
+## Setup
+
+- [Create an discord app & bot](https://discord.com/developers/applications)
+
+- Create a config.json files with
+    > "token": "your_bot_token"
+
+    > "clientId": "you can find it once you have created your discord app "
+
+    > "guildId": "the id of the discord server"
+
+- run npm install to install dep
+
+- run npm start
+
+Thats it ! Bot is logged in !
+
+Write !help in discord server to see bot commands!
